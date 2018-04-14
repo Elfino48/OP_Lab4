cc9fa84a1facc680faea725e5838c71057541810
klerk1t <mr.bogdan123@gmail.com>
2018-04-14 20:39:09 +0300

    qq

---
+++ src/Hash.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mrbog on 14.04.2018.
+ */
+public class Hash {
+    public static void main(String[] args) {
+        System.out.println("Hello, El!");
+        Main.qq();
+    }
+}
