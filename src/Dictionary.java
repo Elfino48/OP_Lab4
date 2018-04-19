f33d87dc4baf4bfa4bfd63e5c1012972cbdf7d1f
klerk1t <mr.bogdan123@gmail.com>
2018-04-19 22:36:21 +0300

    kak-to tak

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,27 +1,31 @@
 import java.io.*;
-import java.util.regex.*;
 
 public class Dictionary {
     String res;
     Dictionary (String res) {
         this.res = res;
     }
-    public void qw() throws IOException {
-        String alphaber = "abcdefghijklmnopqrstuvwxyz";
 
-        String dictionary[] = new String[26];
-        try (FileReader reader = new FileReader(res)) {
-            String stroka = "";
-            int c;
-            while ((c = reader.read()) != -1) {
-                stroka += String.valueOf(c);
-            }
-        }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
-
-
+    public void setDictionary() throws IOException {       // Метод разбивает файл на строки, и передает строки
+        FileInputStream stream = new FileInputStream(res); // методу setDictionary
+        int length = stream.available();
+        byte [] data = new byte[ length ];
+        stream.read(data);
+        stream.close();
+        String text = new String(data);
+        String[] st = text.split("\n");
+        for(String x : st)
+            setDictionary(x);
     }
+
+    public static void setDictionary(String word) {                      // Метод разделяет строку на ключ и значение
+        String key = word.substring(0, word.indexOf(';')).toLowerCase(); // и вызывает метод push из твоего класса
+        String val = word;
+        ManualHash.push(key, val);
+    }
+
+    public String getValue(String key) {  // Метод вызывает твой метод который
+        return ManualHash.hashSearch(key);  // возвращает значение ключа, хотя по мне как-то не очень
+    }                                     // \__( -_-)__/
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-//public class Main {
-   /* public static void main(String[] args) {
-        System.out.println("Js govno :3");
-        System.out.println("Js govno :3");
-        System.out.println("ELMAR's BRANCH");
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ManualHash a = new ManualHash();
+        Dictionary b = new Dictionary("res/dictionary.csv");
+        a.createHashTable();
+        b.setDictionary();
+        System.out.println(b.getValue(args[0].toLowerCase()));
     }
 }
-   */
+//Тут думаю всё понятно
